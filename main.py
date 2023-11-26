8631cf06b29e493b00602252e0dc8ef34ffc23ae
luklukass <104025385+luklukass@users.noreply.github.com>
2023-11-26 09:46:01 +0100

    start to extract content

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from tkinter import ttk
 from tkinter import filedialog
 import re
 import webbrowser
+import ctypes
+
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
 
 class SuricataRuleParser:
     def __init__(self):
         self.keyword_pattern = re.compile(r'content:"([^"]+)"')
 
     def extract_payload_keywords(self, rule_text):
-        """
-        Extract all payload keywords from a Suricata rule.
-        """
         matches = self.keyword_pattern.findall(rule_text)
         return matches
 
@@ -20,54 +21,51 @@ def select_rule(event):
     selected_rule_index = rule_combobox.current()
     if selected_rule_index != -1 and selected_rule_index < len(filtered_rules):
         selected_rule = filtered_rules[selected_rule_index]
-        rule_text.config(state=tk.NORMAL)  # Make the Text widget editable
-        rule_text.delete("1.0", tk.END)  # Clear the Text widget
-        rule_text.insert(tk.END, selected_rule)  # Insert the selected rule
+        rule_text.config(state=tk.NORMAL)
+        rule_text.delete("1.0", tk.END)
+        rule_text.insert(tk.END, selected_rule)
         rule_text.tag_configure("center", justify='center')
-        # Scroll to the top
         rule_text.yview_moveto(0.0)
-
         rule_text.tag_add("center", "1.0", "end")
-        rule_text.config(state=tk.DISABLED)  # Make the Text widget read-only
+        rule_text.config(state=tk.DISABLED)
 
         # Extract all payload keywords
         payload_keywords = suricata_parser.extract_payload_keywords(selected_rule)
         if payload_keywords:
-            # Clear the input_text field
-            input_text.delete("1.0", tk.END)
-            # Insert all payload keywords into the input_text field
-            for keyword in payload_keywords:
-                input_text.insert(tk.END, f"{keyword}\n")
-        else:
-            # If no payload keywords, display a blank input_text field
-            input_text.delete("1.0", tk.END)
-def clear_search(event):
-    if search_entry.get() == "Search":
-        search_entry.delete(0, tk.END)
-        update_combobox_options("")  # Reset the ComboBox to show all rules
+            content_box.config(state=tk.NORMAL)
+            content_box.delete("1.0", tk.END)
 
-# Custom function to filter rules based on search text
+            for keyword in payload_keywords:
+                content_box.insert(tk.END, f"{keyword}\n")
+            content_box.tag_add("center", "1.0", "end")
+            content_box.config(state=tk.DISABLED)
+        else:
+            content_box.config(state=tk.NORMAL)
+            content_box.delete("1.0", tk.END)
+            content_box.config(state=tk.DISABLED)
+
 def filter_rules(search_text):
     matching_msgs = []
-    matching_rules = []  # Store the rules that match the search criteria
+    matching_rules = []
     for rule in rules:
         if search_text.lower() in rule.lower():
-            matching_rules.append(rule)  # Add matching rule to the list
+            matching_rules.append(rule)
             match = re.search(r'msg:"([^"]+)"', rule)
             if match:
                 matching_msgs.append(match.group(1))
     return matching_msgs, matching_rules
 
-# Function to update ComboBox options based on the search text
 def update_combobox_options(search_text):
     matching_msgs, matching_rules = filter_rules(search_text)
     rule_combobox['values'] = matching_msgs
-    global filtered_rules  # Define a global variable to store filtered rules
-    filtered_rules = matching_rules  # Update filtered_rules with matching rules
+    global filtered_rules
+    filtered_rules = matching_rules
 
-# Function to handle "Choose" menu item
+
+
+
 def choose_file_action():
-    global filtered_rules  # Use the global filtered_rules
+    global filtered_rules
     file_path = filedialog.askopenfilename(title="Choose a File")
     if file_path:
         with open(file_path, 'r') as file:
@@ -75,38 +73,43 @@ def choose_file_action():
             msg_values.clear()
             for line in file:
                 if re.search(r'\bsid\b', line):
-                    # Remove leading '#' if it exists
                     cleaned_line = line.lstrip('#').strip()
-                    rules.append(cleaned_line)  # Update the rules list
+                    rules.append(cleaned_line)
                     match = re.search(r'msg:"([^"]+)"', cleaned_line)
                     if match:
                         msg_values.append(match.group(1))
-            filtered_rules = rules.copy()  # Update the global filtered_rules
+            filtered_rules = rules.copy()
             rule_combobox['values'] = msg_values
 
 def open_documentation():
     webbrowser.open('suricata-latest\index.html')
-
 # Create the main window
+
+
 root = tk.Tk()
-root.title("Suricater")
+root.title("SURICATER")
+root.iconbitmap('assets/img/logo.ico')
+root.option_add("*TCombobox*Listbox.font", "Helevetica 10")
 
 # Set the initial window size
-root.geometry("800x500")
-
+root.minsize(900, 600)
+root.geometry(f"1200x900")
+menu_font = ("Helvetica", 10)
 # Create a menu bar
 menu_bar = tk.Menu(root)
 root.config(menu=menu_bar)
 
+
 # Create a File menu
-signatures_menu = tk.Menu(menu_bar, tearoff=0)
-help_menu = tk.Menu(menu_bar, tearoff=0)
+signatures_menu = tk.Menu(menu_bar, tearoff=0, font=menu_font)
+help_menu = tk.Menu(menu_bar, tearoff=0, font=menu_font)
 
 menu_bar.add_cascade(label="Signatures", menu=signatures_menu)
 menu_bar.add_cascade(label="Help", menu=help_menu)
 
+
 # Add items to the File menu
-signatures_menu.add_command(label="Choose", command=choose_file_action)  # Added command
+signatures_menu.add_command(label="Choose", command=choose_file_action)
 signatures_menu.add_command(label="Create")
 help_menu.add_command(label="Documentation", command=open_documentation)
 help_menu.add_command(label="Info")
@@ -115,57 +118,79 @@ help_menu.add_command(label="Info")
 rules = []
 msg_values = []
 suricata_parser = SuricataRuleParser()
-# Store the original unfiltered rules
 filtered_rules = rules.copy()
 
-# Create a frame to hold the search elements
-search_frame = tk.Frame(root)
-search_frame.grid(row=0, column=0, padx=10, pady=10)
+# Create a Label widget for the search title
+search_label = tk.Label(root, text="Search:", font=("Helvetica", 10))
+search_label.grid(row=0, column=0, padx=10, sticky="w")
 
 # Create a search box Entry widget with a placeholder
-search_entry = tk.Entry(search_frame, width=40)
-search_entry.insert(0, "Search")  # Placeholder text
-search_entry.pack(side=tk.LEFT, padx=5)
+search_entry = tk.Entry(root, width=200, font=("Helvetica", 11))
+search_entry.grid(row=1, column=0, padx=10, pady=(0, 13), sticky="w")
 
 # Create a Combobox to select the "msg" option
-rule_combobox = ttk.Combobox(root, values=msg_values, width=77)
-rule_combobox.grid(row=0, column=1, padx=10, pady=10)  # Use grid layout
+rules_label = tk.Label(root, text="Rules:", font=("Helvetica", 10))
+rules_label.grid(row=0, column=1, padx=10, sticky="w")
+rule_combobox = ttk.Combobox(root, values=msg_values, width=230,  font=("Helvetica", 11))
+rule_combobox.grid(row=1, column=1, padx=10, pady=(0, 13), sticky="w")
 
 # Enable the search functionality
 rule_combobox['state'] = 'readonly'
 
-# Create a button to select the rule and place it next to the Combobox
-#select_button = tk.Button(root, text="Select Rule", command=select_rule)
-#select_button.grid(row=0, column=2, padx=20, pady=10)  # Use grid layout
+# Create a Label widget for the selected rule title
+rule_label = tk.Label(root, text="Selected rule:", font=("Helvetica", 10))
+rule_label.grid(row=2, column=0, columnspan=3, padx=10, sticky="w")
 
 # Create a Text widget to display the selected rule
-rule_text = tk.Text(root, wrap=tk.WORD, width=95, height=8)
-rule_text.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+rule_text = tk.Text(root, wrap=tk.WORD, width=400, height=15,  font=("Helvetica", 11))
+rule_text.grid(row=3, column=0, columnspan=3, padx=10, pady=(0, 13))
 rule_text.tag_configure("center", justify='center')
-rule_text.config(state=tk.DISABLED)  # Make the Text widget read-only
+rule_text.config(state=tk.DISABLED)
+
+# Create a Label widget for the content title
+content_label = tk.Label(root, text="Content:", font=("Helvetica", 10))
+content_label.grid(row=4, column=0, padx=10, sticky="w")
+
+# Create a Text widget for the content_box
+content_box = tk.Text(root, wrap=tk.WORD, width=180, height=12, font=("Helvetica", 11))
+content_box.grid(row=5, column=0, padx=10, pady=(0, 10), sticky="w")
+content_box.config(state=tk.DISABLED)
+
+# Create a Label widget for the input payload title
+input_label = tk.Label(root, text="Input payload:", font=("Helvetica", 10))
+input_label.grid(row=6, column=0, columnspan=3, padx=10, sticky="w")
 
 # Create an input field as a Text widget
-input_text = tk.Text(root, wrap=tk.WORD, width=95, height=17)  # Adjust height as needed
-input_text.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+input_text = tk.Text(root, wrap=tk.WORD, width=400, height=16,  font=("Helvetica", 11))
+input_text.grid(row=7, column=0, columnspan=3, padx=10, pady=(0, 10))
 
 # Scrollbar for rule_text
 rule_text_scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=rule_text.yview)
-rule_text_scrollbar.grid(row=1, column=3, sticky='ns')
+rule_text_scrollbar.grid(row=3, column=3, sticky='ns')
 rule_text.config(yscrollcommand=rule_text_scrollbar.set)
+
+# Scrollbar for content_box
+content_box_scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=content_box.yview)
+content_box_scrollbar.grid(row=5, column=3, sticky='ns')
+content_box.config(yscrollcommand=content_box_scrollbar.set)
 
 # Scrollbar for input_text
 input_text_scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=input_text.yview)
-input_text_scrollbar.grid(row=2, column=3, sticky='ns')
+input_text_scrollbar.grid(row=7, column=3, sticky='ns')
 input_text.config(yscrollcommand=input_text_scrollbar.set)
 
-# Bind a function to update ComboBox options when typing in the search box
+# Configure row and column weights to make them resizable
+root.grid_rowconfigure(3, weight=1)
+root.grid_rowconfigure(5, weight=1)
+root.grid_rowconfigure(7, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
+
+# Bind functions to events
 search_entry.bind('<KeyRelease>', lambda event: update_combobox_options(search_entry.get()))
-
-# Bind a function to clear the search box when clicked
-search_entry.bind('<Button-1>', clear_search)
-
-# Bind a function to select a rule and display it when an item in the ComboBox is selected
 rule_combobox.bind('<<ComboboxSelected>>', select_rule)
 
 # Start the GUI event loop
+
 root.mainloop()
