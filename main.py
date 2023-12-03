c224d983aed80424b69a898e100333d3311a3ff0
luklukass <104025385+luklukass@users.noreply.github.com>
2023-12-03 19:56:47 +0100

    add depth

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class SuricataRuleParser:
         self.keyword_nocase = re.compile(r'\bnocase\b')
         self.keyword_startwith = re.compile(r'\bstartwith\b')
         self.keyword_endwith = re.compile(r'\bendwith\b')
+        self.keyword_depth = re.compile(r'\bdepth:\s*(\d+)\b')
 
     def extract_content(self, rule_text):
         matches = self.keyword_content.findall(rule_text)
@@ -29,6 +30,9 @@ class SuricataRuleParser:
     def has_endwith(self, rule_text):
         return bool(self.keyword_endwith.search(rule_text))
 
+    def get_all_depth(self, rule_text):
+        depth_matches = self.keyword_depth.finditer(rule_text)
+        return [int(match.group(1)) for match in depth_matches] if depth_matches else []
 
 # Function to handle rule selection and display it
 # Function to handle rule selection and display it
@@ -50,6 +54,9 @@ def select_rule(event):
         startwith_info = "True" if suricata_parser.has_startwith(selected_rule) else "False"
         endwith_info = "True" if suricata_parser.has_endwith(selected_rule) else "False"
 
+        depth_values = suricata_parser.get_all_depth(selected_rule)
+
+
         content_text = ""
         if payload_keywords:
             for keyword in payload_keywords:
@@ -65,17 +72,27 @@ def select_rule(event):
         nocase_box.config(state=tk.NORMAL)
         nocase_box.delete("1.0", tk.END)
         nocase_box.insert(tk.END, nocase_info)
+        nocase_box.tag_add("center", "1.0", "end")
         nocase_box.config(state=tk.DISABLED)
 
         startwith_box.config(state=tk.NORMAL)
         startwith_box.delete("1.0", tk.END)
         startwith_box.insert(tk.END, startwith_info)
+        startwith_box.tag_add("center", "1.0", "end")
         startwith_box.config(state=tk.DISABLED)
+
 
         endwith_box.config(state=tk.NORMAL)
         endwith_box.delete("1.0", tk.END)
         endwith_box.insert(tk.END, endwith_info)
+        endwith_box.tag_add("center", "1.0", "end")
         endwith_box.config(state=tk.DISABLED)
+
+        depth_box.config(state=tk.NORMAL)
+        depth_box.delete("1.0", tk.END)
+        depth_box.insert(tk.END, "; ".join(map(str, depth_values)))
+        depth_box.tag_add("center", "1.0", "end")
+        depth_box.config(state=tk.DISABLED)
 def filter_rules(search_text):
     matching_msgs = []
     matching_rules = []
@@ -229,11 +246,12 @@ content_box.config(yscrollcommand=content_box_scrollbar.set)
 
 # Create a Label widget for the nocase information
 nocase_label = tk.Label(root, text="Nocase:", font=("Helvetica", 10))
-nocase_label.grid(row=5, column=1, padx=25, sticky="nw")
+nocase_label.grid(row=5, column=1, padx=55, sticky="nw")
 
 # Create a Text widget for the nocase information
-nocase_box = tk.Text(root, wrap=tk.WORD, width=10, height=1, font=("Helvetica", 11))
+nocase_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
 nocase_box.grid(row=5, column=1, pady=30, sticky="nw")
+nocase_box.tag_configure("center", justify='center')
 nocase_box.config(state=tk.DISABLED)
 
 # Create a Label widget for the nocase information
@@ -242,22 +260,34 @@ startwith_label = tk.Label(root, text="Startwith:", font=("Helvetica", 10))
 startwith_label.grid(row=5, column=1, sticky="n")
 
 # Create a Text widget for the startwith information
-startwith_box = tk.Text(root, wrap=tk.WORD, width=10, height=1, font=("Helvetica", 11))
+startwith_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
 startwith_box.grid(row=5, column=1, pady=30, sticky="n")
+startwith_box.tag_configure("center", justify='center')
 startwith_box.config(state=tk.DISABLED)
 
 # Create a Label widget for the endwith information
 endwith_label = tk.Label(root, text="Endwith:", font=("Helvetica", 10))
-endwith_label.grid(row=5, column=1,padx=30,  sticky="ne")
+endwith_label.grid(row=5, column=1,padx=60,  sticky="ne")
 
 # Create a Text widget for the endwith information
-endwith_box = tk.Text(root, wrap=tk.WORD, width=10, height=1, font=("Helvetica", 11))
+endwith_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
 endwith_box.grid(row=5, column=1,pady=30,padx=10, sticky="ne")
+endwith_box.tag_configure("center", justify='center')
 endwith_box.config(state=tk.DISABLED)
+
+# Create a Label widget for the depth information
+depth_label = tk.Label(root, text="Depth:", font=("Helvetica", 10))
+depth_label.grid(row=5, column=1, sticky="w", padx=60)  # Set row to 4 (or any appropriate row index)
+
+# Create a Text widget for the depth information
+depth_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
+depth_box.grid(row=5, column=1, sticky="w",pady=(60,0))  # Set columnspan to 2 to make the box span two columns
+depth_box.tag_configure("center", justify='center')
+depth_box.config(state=tk.DISABLED)
 
 # Scrollbar for input_text
 input_text_scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=input_text.yview)
-input_text_scrollbar.grid(row=7, column=3, sticky='ns')
+input_text_scrollbar.grid(row=7, column=1, sticky='ns')
 input_text.config(yscrollcommand=input_text_scrollbar.set)
 
 # Configure row and column weights to make them resizable
