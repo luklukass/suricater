b98e7fa6fbdd0cf38c3b741546ac7ae069efd52b
luklukass <104025385+luklukass@users.noreply.github.com>
2023-12-09 17:15:31 +0100

    add offset, distance, within, isdataat

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import ctypes
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
-
 class SuricataRuleParser:
     def __init__(self):
         self.keyword_content = re.compile(r'content:"([^"]+)"')
@@ -16,6 +15,10 @@ class SuricataRuleParser:
         self.keyword_startwith = re.compile(r'\bstartwith\b')
         self.keyword_endwith = re.compile(r'\bendwith\b')
         self.keyword_depth = re.compile(r'\bdepth:\s*(\d+)\b')
+        self.keyword_offset = re.compile(r'\boffset:\s*(\d+)\b')
+        self.keyword_distance = re.compile(r'\bdistance:\s*(-?\d+)\b')
+        self.keyword_within = re.compile(r'\bwithin:\s*(\d+)\b')
+        self.keyword_isdataat = re.compile(r'\bisdataat:\s*(!?)(\d+)(?:,(\w+))?(\b|;|\s)')
 
     def extract_content(self, rule_text):
         matches = self.keyword_content.findall(rule_text)
@@ -33,6 +36,23 @@ class SuricataRuleParser:
     def get_all_depth(self, rule_text):
         depth_matches = self.keyword_depth.finditer(rule_text)
         return [int(match.group(1)) for match in depth_matches] if depth_matches else []
+
+    def get_all_offset(self, rule_text):
+        offset_matches = self.keyword_offset.finditer(rule_text)
+        return [int(match.group(1)) for match in offset_matches] if offset_matches else []
+
+    def get_all_distance(self, rule_text):
+        distance_matches = self.keyword_distance.finditer(rule_text)
+        return [int(match.group(1)) for match in distance_matches] if distance_matches else []
+
+    def get_all_within(self, rule_text):
+        within_matches = self.keyword_within.finditer(rule_text)
+        return [int(match.group(1)) for match in within_matches] if within_matches else []
+
+    def get_all_isdataat(self, rule_text):
+        isdataat_matches = self.keyword_isdataat.finditer(rule_text)
+        return [f"{match.group(1)}{match.group(2)}, {match.group(3)}" if match.group(
+            3) else f"{match.group(1)}{match.group(2)}" for match in isdataat_matches] if isdataat_matches else []
 
 # Function to handle rule selection and display it
 # Function to handle rule selection and display it
@@ -53,9 +73,11 @@ def select_rule(event):
         nocase_info = "True" if suricata_parser.has_nocase(selected_rule) else "False"
         startwith_info = "True" if suricata_parser.has_startwith(selected_rule) else "False"
         endwith_info = "True" if suricata_parser.has_endwith(selected_rule) else "False"
-
         depth_values = suricata_parser.get_all_depth(selected_rule)
-
+        offset_values = suricata_parser.get_all_offset(selected_rule)
+        distance_values = suricata_parser.get_all_distance(selected_rule)
+        within_values = suricata_parser.get_all_within(selected_rule)
+        isdataat_values = suricata_parser.get_all_isdataat(selected_rule)
 
         content_text = ""
         if payload_keywords:
@@ -81,7 +103,6 @@ def select_rule(event):
         startwith_box.tag_add("center", "1.0", "end")
         startwith_box.config(state=tk.DISABLED)
 
-
         endwith_box.config(state=tk.NORMAL)
         endwith_box.delete("1.0", tk.END)
         endwith_box.insert(tk.END, endwith_info)
@@ -93,6 +114,30 @@ def select_rule(event):
         depth_box.insert(tk.END, "; ".join(map(str, depth_values)))
         depth_box.tag_add("center", "1.0", "end")
         depth_box.config(state=tk.DISABLED)
+
+        offset_box.config(state=tk.NORMAL)
+        offset_box.delete("1.0", tk.END)
+        offset_box.insert(tk.END, "; ".join(map(str, offset_values)))
+        offset_box.tag_add("center", "1.0", "end")
+        offset_box.config(state=tk.DISABLED)
+
+        distance_box.config(state=tk.NORMAL)
+        distance_box.delete("1.0", tk.END)
+        distance_box.insert(tk.END, "; ".join(map(str, distance_values)))
+        distance_box.tag_add("center", "1.0", "end")
+        distance_box.config(state=tk.DISABLED)
+
+        within_box.config(state=tk.NORMAL)
+        within_box.delete("1.0", tk.END)
+        within_box.insert(tk.END, "; ".join(map(str, within_values)))
+        within_box.tag_add("center", "1.0", "end")
+        within_box.config(state=tk.DISABLED)
+
+        isdataat_box.config(state=tk.NORMAL)
+        isdataat_box.delete("1.0", tk.END)
+        isdataat_box.insert(tk.END, "; ".join(map(str, isdataat_values)))
+        isdataat_box.tag_add("center", "1.0", "end")
+        isdataat_box.config(state=tk.DISABLED)
 def filter_rules(search_text):
     matching_msgs = []
     matching_rules = []
@@ -109,9 +154,6 @@ def update_combobox_options(search_text):
     rule_combobox['values'] = matching_msgs
     global filtered_rules
     filtered_rules = matching_rules
-
-
-
 
 def choose_file_action():
     global filtered_rules
@@ -168,14 +210,12 @@ menu_font = ("Helvetica", 10)
 menu_bar = tk.Menu(root)
 root.config(menu=menu_bar)
 
-
 # Create a File menu
 signatures_menu = tk.Menu(menu_bar, tearoff=0, font=menu_font)
 help_menu = tk.Menu(menu_bar, tearoff=0, font=menu_font)
 
 menu_bar.add_cascade(label="Signatures", menu=signatures_menu)
 menu_bar.add_cascade(label="Help", menu=help_menu)
-
 
 # Add items to the File menu
 signatures_menu.add_command(label="Choose", command=choose_file_action)
@@ -222,7 +262,7 @@ content_label = tk.Label(root, text="Content:", font=("Helvetica", 10))
 content_label.grid(row=4, column=0, padx=10, sticky="w")
 
 # Create a Text widget for the content_box
-content_box = tk.Text(root, wrap=tk.WORD, width=180, height=12, font=("Helvetica", 11))
+content_box = tk.Text(root, wrap=tk.WORD, width=180, height=18, font=("Helvetica", 11))
 content_box.grid(row=5, column=0, padx=10, pady=(0, 10), sticky="w")
 content_box.config(state=tk.DISABLED)
 
@@ -285,6 +325,46 @@ depth_box.grid(row=5, column=1, sticky="w",pady=(60,0))  # Set columnspan to 2 t
 depth_box.tag_configure("center", justify='center')
 depth_box.config(state=tk.DISABLED)
 
+# Create a Label widget for the offset information
+offset_label = tk.Label(root, text="Offset:", font=("Helvetica", 10))
+offset_label.grid(row=5, column=1, padx=60)  # Set row to 4 (or any appropriate row index)
+
+# Create a Text widget for the offset information
+offset_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
+offset_box.grid(row=5, column=1,pady=(60,0))  # Set columnspan to 2 to make the box span two columns
+offset_box.tag_configure("center", justify='center')
+offset_box.config(state=tk.DISABLED)
+
+# Create a Label widget for the distance information
+distance_label = tk.Label(root, text="Distance:", font=("Helvetica", 10))
+distance_label.grid(row=5, column=1,sticky="e", padx=58)  # Set row to 4 (or any appropriate row index)
+
+# Create a Text widget for the distance information
+distance_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
+distance_box.grid(row=5, column=1,sticky="e", pady=(60,0), padx=10)  # Set columnspan to 2 to make the box span two columns
+distance_box.tag_configure("center", justify='center')
+distance_box.config(state=tk.DISABLED)
+
+# Create a Label widget for the within information
+within_label = tk.Label(root, text="Within:", font=("Helvetica", 10))
+within_label.grid(row=5, column=1,sticky="sw", padx=60, pady=30)  # Set row to 4 (or any appropriate row index)
+
+# Create a Text widget for the within information
+within_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
+within_box.grid(row=5, column=1,sticky="sw")  # Set columnspan to 2 to make the box span two columns
+within_box.tag_configure("center", justify='center')
+within_box.config(state=tk.DISABLED)
+
+# Create a Label widget for the isdataat information
+isdataat_label = tk.Label(root, text="Isdataat:", font=("Helvetica", 10))
+isdataat_label.grid(row=5, column=1,sticky="s", padx=60, pady=30)  # Set row to 4 (or any appropriate row index)
+
+# Create a Text widget for the isdataat information
+isdataat_box = tk.Text(root, wrap=tk.WORD, width=15, height=1, font=("Helvetica", 11))
+isdataat_box.grid(row=5, column=1,sticky="s")  # Set columnspan to 2 to make the box span two columns
+isdataat_box.tag_configure("center", justify='center')
+isdataat_box.config(state=tk.DISABLED)
+
 # Scrollbar for input_text
 input_text_scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=input_text.yview)
 input_text_scrollbar.grid(row=7, column=3, sticky='ns')
@@ -303,5 +383,4 @@ search_entry.bind('<KeyRelease>', lambda event: update_combobox_options(search_e
 rule_combobox.bind('<<ComboboxSelected>>', select_rule)
 
 # Start the GUI event loop
-
 root.mainloop()
