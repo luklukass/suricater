49941ac2395f55b26a3d8811405c021572db26f1
luklukass <104025385+luklukass@users.noreply.github.com>
2024-01-07 08:33:56 +0100

    new suricata documentation

--- suricata-latest/_static/documentation_options.js
+++ suricata-latest/_static/documentation_options.js
@@ -1,10 +1,13 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.0.2-dev',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '8.0.0-dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'readthedocssinglehtmllocalmedia',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
